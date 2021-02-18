bb6a279561df29bbc2c1de52ed6888a00b15284c
Juaria Iqbal <55938363+Juaria@users.noreply.github.com>
2021-02-18 14:47:35 +0600

    Update ToDoLists.jsx

--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import CheckIcon from '@material-ui/icons/Check';
+import Button from '@material-ui/core/Button';
 
 const ToDoLists = (props) => {
 
+      
     const [line, setLine] = useState(false);
 
     const cutIt = () => {
         setLine(true);
     };
+
+    const btnClr = "#5eaaa8";
+    const [bg, setBg] = useState(btnClr);
+    const [num, setNum] = useState(1);
+    
+    const incNum = () => {
+        if (num < 10){
+        setNum(num + 1);
+        }
+        else { 
+            setNum (1);
+        }
+    };
+    
   
     return (
     <>
@@ -26,6 +42,11 @@ const ToDoLists = (props) => {
             <li style={{ textDecoration: line ? "line-through" : "none"}}> 
             {props.text} 
             </li> 
+            
+            <button className="priorityBtn" 
+            //onClick={bgChange} 
+            onClick={incNum}> {num} 
+            </button>
         </div>
     </>
     );
