1389e5b70420528d5fdf9481bc306240f650cfc9
Juaria Iqbal <55938363+Juaria@users.noreply.github.com>
2021-02-19 01:05:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ToDoLists from './ToDoLists';
+import { render } from '@testing-library/react';
 
 
 const App = () => {
@@ -9,6 +10,20 @@ const App = () => {
   const [inputList, setInputList] = useState("");
   const [Items, setItems] = useState([]);
 
+////////
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const inputEvent = (e) => {
+    setSearch(e.target.value);
+  };
+  useEffect(() => {
+    const results = Items.filter (Items => Items.toLowerCase().includes(search));
+    setSearchResults(results);
+  }, [search]);
+
+
+//////////
   const itemEvent = (event) => {
     setInputList(event.target.value);
   };
@@ -37,17 +52,30 @@ const App = () => {
         <br />
         <h1> ToDo List </h1>
         <br />
-        <input 
+        <input
           type="text" 
           placeholder="Add items" 
           value= {inputList} 
           onChange={itemEvent} 
         />
         <button onClick={listOfItems}> + </button>
+        <br />
+
+        <div className="search_bar">
+          <input type="text" 
+          placeholder="Search..."
+          value={search}
+          onChange={inputEvent}
+          />
+        
+        </div>
+        {searchResults.map(item => (
+            <ol>{item}</ol>
+          ))}
+
 
         <ol>
           {/*<li> {inputList} </li>*/}
-
           {Items.map((itemval, index) => {
             return <ToDoLists 
             key={index} 
@@ -56,6 +84,7 @@ const App = () => {
             onSelect={deleteItems} 
             />;
           })}
+        
         </ol>
       </div>
     </div>
